fea587f3feb5a5abe1b0fd0371df73bd86c3c2b7
stevebstorm <steve.lorent@gmail.Com>
2024-06-03 15:12:01 +0200

    Correctif Exo 1 (quizz)

---
+++ DemoWebAssembly/Pages/Exercices/Exo1.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace DemoWebAssembly.Pages.Exercices
+{
+    public partial class Exo1
+    {
+        public string Username { get; set; }
+
+        public List<string> Reponses { get; set; }
+
+        public bool GameIsOver { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Reponses = new List<string>();
+        }
+
+        public void RegisterResponse(string response)
+        {
+            Reponses.Add(response);
+            if(Reponses.Count > 2)
+            {
+                GameIsOver = true;
+            }
+        }
+    }
+}
---
+++ DemoWebAssembly/Pages/Exercices/Quizz.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoWebAssembly.Pages.Exercices
+{
+    public partial class Quizz
+    {
+        [Parameter]
+        public string Username { get; set; }
+        [Parameter]
+        public EventCallback<string> ReponseEvent { get; set; }
+        public List<string> Questions { get; set; }
+        public int Counter { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Questions = new List<string>();
+            Questions.Add("Avez vous bien mangé à midi ?");
+            Questions.Add("Appréciez vous BLazor ?");
+            Questions.Add("Envie d'une sieste ?");
+
+            Counter = 0;
+        }
+
+        public void Repondre(string r)
+        {
+            ReponseEvent.InvokeAsync(r);
+            Counter++;
+        }
+    }
+}
