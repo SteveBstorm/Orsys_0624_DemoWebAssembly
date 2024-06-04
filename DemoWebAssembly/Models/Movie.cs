6747a51c1906a23b0d5c1042c28c894ea90776bc
stevebstorm <steve.lorent@gmail.Com>
2024-06-04 09:53:25 +0200

    Formulaire

---
+++ DemoWebAssembly/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoWebAssembly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Le titre est requis")]
+        public string Title { get; set; }
+        /*Microsoft.AspNetCore.Components.DataAnnotation.Validation*/
+        [ValidateComplexType]
+        public Person Realisator { get; set; }
+        [Required]
+        [Range(1977, 2050)]
+        public int ReleaseYear { get; set; }
+
+        public string Synopsis { get; set; }
+        public Movie()
+        {
+            Realisator = new Person();
+        }
+    }
+    
+    public class Person
+    {
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+    }
+}
---
+++ DemoWebAssembly/Pages/Demos/Demo6.razor.cs
@@ -0,0 +1,24 @@
+﻿using DemoWebAssembly.Models;
+
+namespace DemoWebAssembly.Pages.Demos
+{
+    public partial class Demo6
+    {
+        public Movie newMovie { get; set; }
+
+        protected override void OnInitialized()
+        {
+            newMovie = new Movie();
+        }
+
+        public void onValidSubmit()
+        {
+            Console.WriteLine("Formulaire OK");
+        }
+
+        public void onSubmit()
+        {
+            Console.WriteLine("Submit");
+        }
+    }
+}
