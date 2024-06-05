42fa226aa36bc76529a7efd5f5d5a038f72a3f10
stevebstorm <steve.lorent@gmail.Com>
2024-06-05 14:14:02 +0200

    Final commit

--- DemoWebAssembly/Program.cs
+++ DemoWebAssembly/Program.cs
@@ -2,9 +2,12 @@ using DemoWebAssembly;
 using DemoWebAssembly.MiddleWare;
 using DemoWebAssembly.Pages.Exercices.MovieManager;
 using DemoWebAssembly.Security;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -29,4 +32,46 @@ builder.Services.AddHttpClient("withToken", sp =>
 
 builder.Services.AddScoped<HttpClient>(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("withToken"));
 
-await builder.Build().RunAsync();
+#region Config I18N
+// Spécifier le dossier ou se trouvent les RESX
+builder.Services.AddLocalization(option => option.ResourcesPath = "Resources");
+
+// Config des cultures autorisées dans l'app
+// Ne pas oublier d'ajouter 
+// <BlazorWebAssemblyLoadAllGlobalizationData>true</BlazorWebAssemblyLoadAllGlobalizationData>
+// Dans le csproj
+// Créer un composant CultureSelector
+builder.Services.Configure<RequestLocalizationOptions>(o =>
+{
+    List<CultureInfo> cultureInfos = new List<CultureInfo>()
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("fr-FR")
+    };
+
+    o.SetDefaultCulture("fr-FR");
+    o.SupportedCultures = cultureInfos;
+    o.SupportedUICultures = cultureInfos;
+});
+#endregion
+
+//await builder.Build().RunAsync();
+
+WebAssemblyHost host = builder.Build();
+
+CultureInfo cultureInfo;
+IJSRuntime js = host.Services.GetRequiredService<IJSRuntime>();
+string currentCulture = await js.InvokeAsync<string>("localStorage.getItem", "blazorCulture");
+
+if (currentCulture is not null)
+    cultureInfo = new CultureInfo(currentCulture);
+else
+{
+    cultureInfo = new CultureInfo("fr-FR");
+    await js.InvokeVoidAsync("localStorage.setItem", "blazorCulture", "fr-FR");
+}
+
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+await host.RunAsync();
